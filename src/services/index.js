c02799e255c20a7861c923b67e6c63e6fc1255c2
reforco-dev <reforcodev.atendimento@gmail.com>
2026-01-22 04:50:52 -0300

    updates

--- src/services/index.js
+++ src/services/index.js
@@ -2,8 +2,8 @@ import { QueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
 export const AXIOS = axios.create({
-    baseURL: "http://localhost:8000"
-    // baseURL: "https://animale-back.onrender.com"
+    // baseURL: "http://localhost:8000"
+    baseURL: "https://animale-back.onrender.com"
 });
 
 export const QUERYCLIENT = new QueryClient();
