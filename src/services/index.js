baaf453062e01822f21a9f49530d3394486821ce
Cauan <cauanvictor0325@gmail.com>
2026-02-02 15:21:43 -0300

    feat: Adicionando rota correta da API

--- src/services/index.js
+++ src/services/index.js
@@ -2,8 +2,7 @@ import { QueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
 export const AXIOS = axios.create({
-    // baseURL: "http://localhost:8000"
-    baseURL: "https://animale-back.onrender.com"
+  baseURL: "/api",
 });
 
 export const QUERYCLIENT = new QueryClient();
